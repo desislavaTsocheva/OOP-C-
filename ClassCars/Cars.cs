a80b6c79cf2d0726800d37848aa50c655868344e
DesislavaTsocheva <desislava_tsocheva@abv.bg>
2023-02-22 16:58:42 +0200

    Class Cars

---
+++ ClassCars/Cars.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCars
+{
+    internal class Cars
+    {
+        private string brand;
+        private string color;
+        private int horsepower;
+        private int price;
+
+        public string Brand
+        {
+            get { return this.brand; }
+            set { this.brand = value; }
+        }
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+        public int Horsepower
+        {
+            get { return this.horsepower; }
+            set { this.horsepower = value; }
+        }
+        public int Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+        public Cars(string brand,string color, int horsepower, int price)
+        {
+            this.brand = Brand;
+            this.color = Color;
+            this.horsepower = Horsepower;
+            this.price = Price;
+        }
+        public override string ToString()
+        {
+            return $"Name:{brand},color:{color},horsepower:{horsepower},price$:{price}";
+        }
+    }
+}
---
+++ ClassCars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars car1 = new Cars("Audi A6", "red", 150, 20000);
+            Cars car2 = new Cars(Console.ReadLine(), Console.ReadLine(), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            Console.WriteLine(car1.ToString());
+            Console.WriteLine(car2.ToString());
+        }
+    }
+}
