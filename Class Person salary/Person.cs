a67ada5593b9d103859b277bbfac7792d3da56b8
DesislavaTsocheva <desislava_tsocheva@abv.bg>
2023-03-31 19:56:10 +0300

    Increase person salary

---
+++ Class Person salary/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Person_salary
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+        private double salary;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+        public Person(string name, int age, double salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;   
+        }
+        public void IncreaseSalary(double percent)
+        {
+            if (age>30)
+            {
+                this.salary += salary * percent / 100;
+            }
+            else
+            {
+                this.salary+=salary * percent / 200;
+            }
+        }
+    }
+}
---
+++ Class Person salary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Person_salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter count persons");
+            int countPersons = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < countPersons; i++)
+            {
+                var p = Console.ReadLine().Split();
+                Person p1 = new Person(p[0], int.Parse(p[1]), double.Parse(p[2]));
+                persons.Add(p1);
+            }
+            double bonus = double.Parse(Console.ReadLine());
+            persons.ForEach(x => Console.WriteLine(x.ToString()));
+            persons.OrderBy(x => x.ToString());
+        }
+    }
+}
